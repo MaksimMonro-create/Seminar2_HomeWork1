dfebbddd60be4906c1f0f806d62d05e74d3ce271
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-27 23:19:54 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// ДАНО ЧИСЛО, ОБОЗНАЧАЮЩЕЕ ДЕНЬ НЕДЕЛИ. ВЫЯСНИТЬ, ЯВЛЯЕТСЯ НОМЕР ДНЯ НЕДЕЛИ ВЫХОДНЫМ.
+
+Console.WriteLine("Введите номер дня недели: 1, 2, 3, 4, 5, 6, 7");
+//int number = new Random().Next(1, 7);
+int number = int.Parse(Console.ReadLine());
+if(number >= 6)
+{
+    Console.WriteLine("Сегодня выходной день недели");
+}
+else
+{
+    Console.WriteLine("Сегодня рабочий день недели");
+}
