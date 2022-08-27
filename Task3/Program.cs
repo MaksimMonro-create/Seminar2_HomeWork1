561c64a4537f101ef5e86778dc4983c4bb922eaa
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-27 23:20:56 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// ЗАДАТЬ НОМЕР ЧЕТВЕРТИ. ПОКАЗАТЬ ДИАПАЗОНЫ ДЛЯ ВОЗМОЖНЫХ КООРДИНАТ
+
+Console.WriteLine("Введите номер четверти: 1, 2, 3 или 4");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+{
+    Console.WriteLine("Выбрана первая четверть.");
+    Console.WriteLine("Диапазоны возможных координат: ");
+    Console.WriteLine("X > 0");
+    Console.WriteLine("Y > 0");
+}
+if(number == 2)
+{
+    Console.WriteLine("Выбрана вторая четверть.");
+    Console.WriteLine("Диапазоны возможных координат: ");
+    Console.WriteLine("X < 0");
+    Console.WriteLine("Y > 0");
+}
+if(number == 3)
+{
+    Console.WriteLine("Выбрана третья четверть.");
+    Console.WriteLine("Диапазоны возможных координат: ");
+    Console.WriteLine("X < 0");
+    Console.WriteLine("Y < 0");
+}
+if(number == 4)
+{
+    Console.WriteLine("Выбрана четвертая четверть.");
+    Console.WriteLine("Диапазоны возможных координат: ");
+    Console.WriteLine("X > 0");
+    Console.WriteLine("Y < 0");
+}
